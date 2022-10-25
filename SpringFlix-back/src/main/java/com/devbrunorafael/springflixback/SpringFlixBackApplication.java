7d37dc30c24feef7c1bb85650e7b0844f57358f6
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-10-25 16:47:32 -0300

    first commit

---
+++ SpringFlix-back/src/main/java/com/devbrunorafael/springflixback/SpringFlixBackApplication.java
@@ -0,0 +1,13 @@
+package com.devbrunorafael.springflixback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFlixBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFlixBackApplication.class, args);
+    }
+
+}
---
+++ SpringFlix-back/src/test/java/com/devbrunorafael/springflixback/SpringFlixBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbrunorafael.springflixback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlixBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
